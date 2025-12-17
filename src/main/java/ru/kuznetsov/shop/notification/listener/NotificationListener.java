69fad46052779520d37e613186957a1c6bd1d3a5
Pavel Kuznetsov <Technic47@gmail.com>
2025-12-17 16:49:17 +0300

    Bug fix.

--- src/main/java/ru/kuznetsov/shop/notification/listener/NotificationListener.java
+++ src/main/java/ru/kuznetsov/shop/notification/listener/NotificationListener.java
@@ -1,6 +1,9 @@
 package ru.kuznetsov.shop.notification.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.kuznetsov.shop.notification.service.NotificationService;
@@ -13,9 +16,22 @@ import static ru.kuznetsov.shop.represent.common.KafkaConst.SELLER_NOTIFICATION_
 public class NotificationListener {
 
     private final NotificationService notificationService;
+    private final ObjectMapper objectMapper;
+
+    Logger logger = LoggerFactory.getLogger(NotificationListener.class);
 
     @KafkaListener(topics = SELLER_NOTIFICATION_TOPIC, groupId = "${spring.kafka.consumer.group-id}")
-    public void saveNotification(SellerNotificationDto notificationDto) {
-        notificationService.addNotification(notificationDto);
+    public void saveNotification(String notificationDtoJson) {
+        logger.info("Saving notification: {}", notificationDtoJson);
+
+        try {
+            notificationService.addNotification(
+                    objectMapper.readValue(notificationDtoJson, SellerNotificationDto.class)
+            );
+
+            logger.info("Notification has been saved successfully");
+        } catch (Exception e) {
+            logger.error("Error while saving notification", e);
+        }
     }
 }
