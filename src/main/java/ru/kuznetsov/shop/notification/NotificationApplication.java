dfcf4f2bef7174f1b31d29ea51f8aacb341019a8
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-15 18:31:24 +0300

    Eureka discovery connection added.

--- src/main/java/ru/kuznetsov/shop/notification/NotificationApplication.java
+++ src/main/java/ru/kuznetsov/shop/notification/NotificationApplication.java
@@ -2,10 +2,12 @@ package ru.kuznetsov.shop.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.business.access.config.DataAccessConfig;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(DataAccessConfig.class)
 public class NotificationApplication {
 
